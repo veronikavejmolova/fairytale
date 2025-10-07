bbdd08b6e21f8226ba3f246976f2a3989442237b
veronikavejmolova <539700@mail.muni.cz>
2025-10-07 12:37:13 +0200

    welcome page

--- fairy/server/app.py
+++ fairy/server/app.py
@@ -1,11 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from starlette.responses import HTMLResponse
+from starlette.templating import Jinja2Templates
 
 from fairy.llm.generator import generate
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
+templates = Jinja2Templates(directory="frontend")
+
+
+@app.get("/api/generate")
+async def api_generate():
     return {"message": generate("hello")}
+
+
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse(
+        "index.html",  # správný pořádek parametrů
+        {"request": request}
+    )
 
