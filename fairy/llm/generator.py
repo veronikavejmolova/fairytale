cf47beff0f0af91f52b5f6503470ed7efa50ff3d
averzeo <abriolova@gmail.com>
2025-10-18 19:41:44 +0200

    new summary result page

--- fairy/llm/generator.py
+++ fairy/llm/generator.py
@@ -4,7 +4,7 @@ def generate(tema_pohadky: str) -> str:
 
     final_prompt = f"""
     Napiš mi poučnou větu o jedenácti slovech, na toto téma: "{tema_pohadky}".
-    Odpověď vrať jako HTML text."
+    Odpověď vrať jako text."
     """
 
     vygenerovany_pribeh_html = call_llm(final_prompt.strip())
