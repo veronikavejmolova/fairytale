f8041ce51f0688ae6a3d77019c97f105d9fbc7c8
averzeo <abriolova@gmail.com>
2025-11-04 12:07:32 +0100

    WIP

--- fairy/main.py
+++ fairy/main.py
@@ -1,5 +1,9 @@
 from fairy.server.app import app
+from fairy.text2speech.tts_server import app as tts_app
 import uvicorn
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("tts_app", host="0.0.0.0", port=8000, reload=True)
--- fairy/text2speech/config.py
+++ fairy/text2speech/config.py
@@ -4,7 +4,8 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 
-load_dotenv(Path(__file__).with_name(".env"))
+#load_dotenv(Path(__file__).with_name(".env"))
+load_dotenv(Path(__file__).parent.parent.parent / ".env")
 
 ELEVENLABS_API_KEY = os.environ["ELEVENLABS_API_KEY"]
 ELEVENLABS_VOICE_ID = "piwFF76q4v4xA9Wyxu1R"
